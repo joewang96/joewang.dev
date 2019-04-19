c4dc700fa4de83db571f1a7ee232a45e335a12e1
Joseph Wang <wang.jo@husky.neu.edu>
2019-04-19 19:26:04 -0400

    add page fade in

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,53 +1,46 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import Header from './Header';
+import './layout.css';
+import Footer from './Footer';
 
-import Header from "./Header"
-import "./layout.css"
-import Footer from "./Footer"
+const Content = styled.div`
+  opacity: ${({ mounted }) => (mounted ? 1 : 0)};
+  transition: 500ms opacity ease-in-out;
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
+  margin: 0 auto;
+  max-width: 1440px;
+`;
+
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mounted: false,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ mounted: true }));
+  }
+
+  render() {
+    const { children } = this.props;
+    const { mounted } = this.state;
+    return (
+      <Content mounted={mounted}>
         <Header />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 1440,
-          }}
-        >
-          <main>{children}</main>
-          <Footer />
-          {/* <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer> */}
-        </div>
-      </>
-    )}
-  />
-)
+        <main>{children}</main>
+        <Footer />
+      </Content>
+    );
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
