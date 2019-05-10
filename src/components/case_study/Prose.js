2a3452f249498a18daa72675578828a467122653
Joseph Wang <wang.jo@husky.neu.edu>
2019-05-10 19:54:59 -0400

    shorten line height for prose

--- src/components/case_study/Prose.js
+++ src/components/case_study/Prose.js
@@ -1,12 +1,10 @@
 import styled from 'styled-components';
-
-import { SIZES } from '../../tokens';
 import P from '../../elements/P';
 
 const Prose = styled(P)`
   font-size: 1.125rem;
   font-family: 'Noto Serif', Georigia, serif;
-  line-height: 2;
+  line-height: 1.9;
 
   & + & {
     margin-top: 24px;
